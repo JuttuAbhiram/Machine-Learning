8d6f2078a65d9bc689ccfc7f4811df0380e554f4
JuttuAbhiram <juttuabhiram@gmail.com>
2026-09-07 16:54:18 +0530

    feat:seperate features and target

--- Linear_Regression/linear_regression.py
+++ Linear_Regression/linear_regression.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-# create a smaple dataset
+# create a sample dataset
 data={"semisters":[1,2,3,4,5,6,7,8],
       "percentage":[60,65,67,70,72,73,75,79]
       }
@@ -16,3 +16,8 @@ df=pd.DataFrame(data)
 
 print("Dataset")
 print(df)
+
+# seperate features (X) and target(y)
+X=df[["semisters"]]
+y=df[["percentage"]]
+
