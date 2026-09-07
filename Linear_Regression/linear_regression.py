058d68c0bec3760f3a3d962e4fe9cb7f0c964e63
JuttuAbhiram <juttuabhiram@gmail.com>
2026-09-07 15:33:09 +0530

    feat: Import Scikit-learn packages for Linear regression model

--- Linear_Regression/linear_regression.py
+++ Linear_Regression/linear_regression.py
@@ -2,3 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
