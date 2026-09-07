c5b3e9894f01c13ba353281dc473166d290b19b6
JuttuAbhiram <juttuabhiram@gmail.com>
2026-09-07 18:24:42 +0530

    feat:spliting the data into training and testing

--- Linear_Regression/linear_regression.py
+++ Linear_Regression/linear_regression.py
@@ -8,8 +8,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 # create a sample dataset
-data={"semisters":[1,2,3,4,5,6,7,8],
-      "percentage":[60,65,67,70,72,73,75,79]
+data={"semisters":[1,2,3,4,5,6,7],
+      "percentage":[60,65,67,70,72,73,75]
       }
 
 df=pd.DataFrame(data)
@@ -19,5 +19,15 @@ print(df)
 
 # seperate features (X) and target(y)
 X=df[["semisters"]]
-y=df[["percentage"]]
+y=df["percentage"]
+
+# Spliting the dataset into training and testing
+x_train,x_test,y_tain,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+print("\nTraining Data:")
+print(x_train)
+
+
+print("\nTesting Data:")
+print(x_test)
 
