e79adc62cfcd39f31f76b693d0b55ef164a9bf88
JuttuAbhiram <juttuabhiram@gmail.com>
2026-09-08 22:39:53 +0530

    feat:Update sample dataset

--- Linear_Regression/linear_regression.py
+++ Linear_Regression/linear_regression.py
@@ -8,8 +8,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 # create a sample dataset
-data={"semisters":[1,2,3,4,5,6,7],
-      "percentage":[60,65,67,70,72,73,75]
+data={"hours":[1,2,3,4,5,6,7,8,9,10],
+      "drill_meters":[6,10,14,19,25,30,37,43,49,56]
       }
 
 df=pd.DataFrame(data)
@@ -18,8 +18,8 @@ print("Dataset")
 print(df)
 
 # seperate features (X) and target(y)
-X=df[["semisters"]]
-y=df["percentage"]
+X=df[["hours"]]
+y=df["drill_meters"]
 
 # Spliting the dataset into training and testing
 x_train,x_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
